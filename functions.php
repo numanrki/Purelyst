bb5d311c506777418f601c862efb2b635718e9bf
Numan Rasheed <bhatti3742@gmail.com>
2026-01-21 12:13:01 +0500

    Bump theme version to 1.0.26; add Material Symbols Icons styles

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define theme constants
  */
-define( 'PURELYST_VERSION', '1.0.25' );
+define( 'PURELYST_VERSION', '1.0.26' );
 define( 'PURELYST_DIR', get_template_directory() );
 define( 'PURELYST_URI', get_template_directory_uri() );
 
